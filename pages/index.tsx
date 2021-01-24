ed282dc7fd08f908c3c9c852e66d15cd17e4af21
Timur00Kh <32297275+Timur00Kh@users.noreply.github.com>
2021-01-25 00:41:45 +0300

    06. SixthSection

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,13 @@ import { FirstSection } from '@/segments/01. FirstSection/FirstSection';
 import { SecondSection } from '@/segments/02. SecondSection/SecondSection';
 import { ThirdSections } from '@/segments/03. ThirdSections/ThirdSections';
 import { FourthAndFifthSection } from '@/segments/04. FourthAndFithSection/FourthAndFifthSection';
+import { SixthSection } from '@/segments/06. SixthSection/SixthSection';
 
 export default function Home(): JSX.Element {
   return (
     <>
       <FirstSection /> <SecondSection /> <ThirdSections />{' '}
-      <FourthAndFifthSection />
+      <FourthAndFifthSection /> <SixthSection />
     </>
   );
 }
