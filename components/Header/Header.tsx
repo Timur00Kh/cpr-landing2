3a46278163d3aab3c289622e91f2486b13b2f757
Timur00Kh <32297275+Timur00Kh@users.noreply.github.com>
2021-01-25 00:43:28 +0300

    Header fix

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -13,26 +13,28 @@ export function Header(): JSX.Element {
 
   return (
     <header className={classes.header}>
-      <Logo />
-      <div className={classes.icon_flex}>
-        <div>
-          <FreeIcon />
-          <h6 className={classes.icon_header}>Бесплатный расчёт</h6>
+      <div className={classes.header_inner}>
+        <Logo />
+        <div className={classes.icon_flex}>
+          <div>
+            <FreeIcon />
+            <h6 className={classes.icon_header}>Бесплатный расчёт</h6>
+          </div>
+          <div>
+            <GuaranteeIcon />
+            <h6 className={classes.icon_header}>Увеличенная гарантия</h6>
+          </div>
+          <div>
+            <PrepaymentIcon />
+            <h6 className={classes.icon_header}>Минимальный аванс</h6>
+          </div>
         </div>
-        <div>
-          <GuaranteeIcon />
-          <h6 className={classes.icon_header}>Увеличенная гарантия</h6>
+        <div className={classes.button_wrapper}>
+          <h5 className={classes.number}>
+            <a href={`tel:${phone.replace(' ', '')}`}>8 800 7777777</a>
+          </h5>
+          <Button block>заказать звонок</Button>
         </div>
-        <div>
-          <PrepaymentIcon />
-          <h6 className={classes.icon_header}>Минимальный аванс</h6>
-        </div>
-      </div>
-      <div className={classes.button_wrapper}>
-        <h5 className={classes.number}>
-          <a href={`tel:${phone.replace(' ', '')}`}>8 800 7777777</a>
-        </h5>
-        <Button block>заказать звонок</Button>
       </div>
     </header>
   );
