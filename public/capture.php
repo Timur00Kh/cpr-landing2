00b385cabba6fe87a196d137875ba28b0fa625dc
Timur00Kh <32297275+Timur00Kh@users.noreply.github.com>
2021-01-25 03:05:18 +0300

    Create capture.php

---
+++ public/capture.php
@@ -0,0 +1,35 @@
+<?php
+$to = "info@cpr-team.ru" ;
+$landingUrl = 'cpr-team.com';
+$headers = "From: landing@$landingUrl";
+
+if (!empty($_POST)) {
+    $name = trim(strip_tags($_POST['name']));
+    $email = trim(strip_tags($_POST['email']));
+    $phone = trim(strip_tags($_POST['phone']));
+    $text = trim(strip_tags($_POST['text']));
+    if ($phone || $email) {
+        if (!$text) {
+            $text = "Пользователь оставил заявку на звонок " . date('Y-m-d H:i:s');
+        }
+        $message = "Запрос пользователя с $landingUrl\n";
+        if ($phone) {
+            $message .= "Телефон: $phone\n";
+        }
+        if ($email) {
+            $message .= "E-mail: $email\n";
+        }
+        if ($name) {
+            $message .= "Имя пользователя: $name\n";
+        }
+        $message .= "Сообщение: \n $text\n";
+        mail($to, "Запрос пользователя с $landingUrl", $message, $headers);
+        echo 'OK';
+    } else {
+        $error = 'Введите e-mail или телефон';
+        echo $error;
+    }
+    die;
+}
+
+
