1217536927efd4836b3b444d4f96ff42ae5775b0
Timur00Kh <32297275+Timur00Kh@users.noreply.github.com>
2021-01-26 04:40:36 +0300

    Update EighthSection.tsx
    Fix carousel bug

--- segments/08. EighthSection/EighthSection.tsx
+++ segments/08. EighthSection/EighthSection.tsx
@@ -1,15 +1,21 @@
 import React, { useEffect, useState } from 'react';
-import { Slider } from '@/components/src';
+// import { Slider } from '@/components/src';
 import img1 from '@/segments/08. EighthSection/images/buildar_logo.png';
 import img2 from '@/segments/08. EighthSection/images/itpark_logo.png';
 import img3 from '@/segments/08. EighthSection/images/logo_dar.png';
 import img4 from '@/segments/08. EighthSection/images/logo_energo.png';
+import dynamic from 'next/dynamic';
 import classes from './EighthSection.module.scss';
+
+const Slider = dynamic(() => import('@/components/src').then((e) => e.Slider));
 
 export const EighthSection: React.FC = () => {
   const [screenWidth, setSw] = useState<number>(1200);
+  const [showSlider, setShowSlider] = useState<boolean>(false);
 
   useEffect(() => {
+    setShowSlider(true);
+
     const callback = () => setSw(window.innerWidth);
     window.addEventListener('resize', callback);
     callback();
@@ -20,18 +26,21 @@ export const EighthSection: React.FC = () => {
   }, []);
 
   const settings = {
-    // autoplay: true,
     centerMode: true,
-    dots: true,
-    dotsScroll: 2,
+    // dots: true,
+    // dotsScroll: 2,
     // gutter: 30,
     slidesToShow: screenWidth > 900 ? 3 : 1
-    // adaptiveHeight: true
   };
+
+  if (!showSlider) {
+    return '';
+  }
 
   return (
     <section className={classes.section8}>
       <h2>ПАРТНЕРЫ</h2>
+      {/* @ts-ignore */}
       <Slider {...settings} className={classes.slider}>
         <div>
           <div className={classes.logo_wrap}>
