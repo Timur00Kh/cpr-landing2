10ed896d2556638a4971d53c146125225d77d635
Timur00Kh <32297275+Timur00Kh@users.noreply.github.com>
2021-01-28 01:15:48 +0300

    add policy routing

--- pages/[[...slug]].tsx
+++ pages/[[...slug]].tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { FirstSection } from '@/segments/01. FirstSection/FirstSection';
 import { SecondSection } from '@/segments/02. SecondSection/SecondSection';
 import { ThirdSections } from '@/segments/03. ThirdSections/ThirdSections';
@@ -8,15 +8,30 @@ import { SeventhSection } from '@/segments/07. SeventhSection/SeventhSection';
 import { EighthSection } from '@/segments/08. EighthSection/EighthSection';
 import { Head } from '@/components/Head';
 import { data } from '@/data';
+import { useRouter } from 'next/router';
 
 export default function Home(): JSX.Element {
+  const router = useRouter();
+  const pol = useMemo(() => {
+    if (router.query?.slug?.length) {
+      return router.query.slug[0] === 'policy';
+    }
+    return false;
+  }, [router.query]);
   return (
     <>
-      <Head
-        title={data.title}
-        description={data.description}
-        keywords={data.keywords}
-      />
+      {pol ? (
+        <Head
+          title="Политика конфиденциальности"
+          description="Данная Политика Конфиденциальности применима ко всей личной информации и полученными или использованными данными на настоящем сайте и всех сайтах, на которые ссылается настоящий сайт."
+        />
+      ) : (
+        <Head
+          title={data.title}
+          description={data.description}
+          keywords={data.keywords}
+        />
+      )}
       <FirstSection /> <SecondSection /> <ThirdSections />{' '}
       <FourthAndFifthSection /> <SixthSection /> <SeventhSection />{' '}
       <EighthSection />
